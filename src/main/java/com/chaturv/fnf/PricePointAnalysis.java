a345a34d8420d0a722037c68a2ebd76c6a049a66
Vineet Chaturvedi <vineet_chaturvedi@hotmail.com>
2020-07-03 23:46:50 +0800

    new stuff

--- src/main/java/com/chaturv/fnf/PricePointAnalysis.java
+++ src/main/java/com/chaturv/fnf/PricePointAnalysis.java
@@ -2,6 +2,8 @@ package com.chaturv.fnf;
 
 import com.google.common.collect.Sets;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -14,32 +16,49 @@ public class PricePointAnalysis {
     static Set<Integer> QUANTITIES = Stream.of(5, 10, 15).collect(Collectors.toSet());
     static List<List<Integer>> qtyCombinations = new ArrayList<>();
 
-    void analyze(Set<Product> products) {
+    void analyze(Set<Product> products) throws IOException {
         products.forEach(System.out::println);
         printLine();
-        IntStream.range(1, 3).forEach( i -> {
+
+        FileWriter fileWriter = new FileWriter("C:/work/fnf_price_analysis.csv");
+        fileWriter.write("Product Combination, Total Sales Price, Total Profit");
+        fileWriter.write("\n");
+
+        IntStream.range(1, products.size() + 1).forEach( i -> {
                     Set<Set<Product>> productCombinations = Sets.combinations(products, i);
                     findQtyCombinations(QUANTITIES, i, new ArrayList<>());
 
                     for (Set<Product> productCombination : productCombinations) {
                         List<Product> tempPC = new ArrayList<>(productCombination);
-                        StringBuilder sb = new StringBuilder();
+
                         for (List<Integer> qtyCombination : qtyCombinations) {
+                            StringBuilder sb = new StringBuilder();
+                            double totalProfit = 0;
+                            double totalSalesPrice = 0;
+
                             for (i = 0; i < tempPC.size(); i++) {
                                 Product product = tempPC.get(i);
                                 Integer qty = qtyCombination.get(i);
 
-                                sb.append(String.format("ProductID=[%s], Qty=[%d] | ", product.getId(), qty));
-
-                                String msg = String.format("ProductID [%s], Qty=[%d], Profit=[%.2f]", product.getId(), qty, product.getProfit() * qty);
-                                System.out.println(msg);
+                                sb.append(String.format("ProductID=[%s] Qty=[%d]; ", product.getId(), qty));
+                                totalProfit += product.getProfit() * qty;
+                                totalSalesPrice += product.getSalesPrice() * qty;
+                            }
+                            sb.append(",").append(totalSalesPrice).append(",").append(String.format("%.2f", totalProfit));
+                            try {
+                                fileWriter.write(sb.toString());
+                                fileWriter.write("\n");
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
                             }
                         }
                     }
+                    System.out.println("Completed productCombination: " + i);
                     qtyCombinations.clear();
                     printLine();
                 }
         );
+        fileWriter.close();
     }
 
     private void printLine() {
